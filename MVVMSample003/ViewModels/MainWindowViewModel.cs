4e15303253acf650317163eb63c0606295bdd809
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-18 18:49:22 +0900

    Add files via upload

---
+++ MVVMSample003/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using Microsoft.Toolkit.Mvvm.Messaging;
+
+namespace MVVMSample003.ViewModels
+{
+    class MainWindowViewModel : ObservableObject
+    {
+        private string _sendMessage;
+        public string SendMessage
+        {
+            get => _sendMessage;
+            set
+            {
+                SetProperty(ref _sendMessage, value);
+                SendCommand.NotifyCanExecuteChanged();
+            }
+        }
+
+        private string _receiveMessage;
+        public string ReceiveMessage
+        {
+            get => _receiveMessage;
+            set => SetProperty(ref _receiveMessage, value);
+        }
+
+        public IRelayCommand SendCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            WeakReferenceMessenger.Default.Register<string>(this, OnReceive);
+
+            SendCommand = new RelayCommand(
+                execute:() =>
+                {
+                    WeakReferenceMessenger.Default.Send(SendMessage);
+                },
+                canExecute:() =>
+                {
+                    return !string.IsNullOrEmpty(SendMessage);
+                });
+        }
+
+        private void OnReceive(object recipient, string message)
+        {
+            ReceiveMessage = $"Received:{message}";
+        }
+    }
+}
---
+++ MVVMSample003/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSample003.ViewModels;
+using System.Windows;
+
+namespace MVVMSample003.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
